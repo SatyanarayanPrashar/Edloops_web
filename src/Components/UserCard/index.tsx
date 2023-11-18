793ac98e5069ecd71f73129ea8454c065832171c
vikas-shivah <vikas@shivawebtech.com>
2023-11-18 14:08:02 +0530

    Added referral code modal

--- src/Components/UserCard/index.tsx
+++ src/Components/UserCard/index.tsx
@@ -1,60 +1,92 @@
-import { publicRoutes } from '@/enums/route.enum';
-import React from 'react';
+import { publicRoutes } from "@/enums/route.enum";
+import React, { useEffect, useState } from "react";
 import NextLink from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { updateReferralModal } from "@/redux/ui/ui.action";
+import { RootState } from "@/redux";
+import { get } from "lodash";
+import { useRouter } from "next/router";
 
 interface UserCardProps {
-    name: string;
-    coins: string;
-    referral: string;
-    enrolledcourses: string[]; // Change the type to string array
+  name: string;
+  coins: string;
+  referral: string;
+  enrolledcourses: string[]; // Change the type to string array
 }
 
 const courseNameMapping: Record<string, string> = {
-    '6': 'Web3',
-    '8': 'Flutter',
-    '12': 'System Design',
-    '13': 'Data Science',
-    // Add more mappings as needed
+  "6": "Web3",
+  "8": "Flutter",
+  "12": "System Design",
+  "13": "Data Science",
+  // Add more mappings as needed
 };
 
 const UserCard: React.FC<UserCardProps> = ({
-    name,
-    coins,
-    referral,
-    enrolledcourses,
+  name,
+  coins,
+  referral,
+  enrolledcourses,
 }) => {
-    return (
-        <div>  
-        <div className='userCard'>
-            <div className="user-name">
-                <div className="svg-feature"><img src="/svg/fire.svg" /> 7 day Streak</div>
-                <div className="svg-feature"><img src="/svg/user.svg" /> {name}</div>
-                <div className="svg-feature"><img src="/svg/coin.svg" /> {coins} Edcoins</div>
-                <a href="   POP UP displaying referral code and a copy bttn ">
-                    <div className="svg-feature"><img src="/svg/referral.svg" /> Refer Friends</div>
-                </a>
-                {/* <div className="svg-feature"><img src="/svg/referral.svg" /> <h6>{referral}</h6> Referral code</div> */}
-                <div className="svg-feature"><img src="/svg/course.svg" /> Enrolled Courses:</div>
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [setShowButton, setSetShowButton] = useState(false);
+  const showModal = () => {
+    dispatch(updateReferralModal({ show: true }));
+  };
+  const surveyInfo = useSelector((state: RootState) => state.uiState.userInfo);
+  useEffect(() => {
+    setSetShowButton(get(surveyInfo, "referral_used", false));
+  }, [router.isReady, surveyInfo]);
+
+  return (
+    <div>
+      <div className="userCard">
+        <div className="user-name">
+          <div className="svg-feature">
+            <img src="/svg/fire.svg" /> 7 day Streak
+          </div>
+          <div className="svg-feature">
+            <img src="/svg/user.svg" /> {name}
+          </div>
+          <div className="svg-feature">
+            <img src="/svg/coin.svg" /> {coins} Edcoins
+          </div>
+          <a href="   POP UP displaying referral code and a copy bttn ">
+            <div className="svg-feature">
+              <img src="/svg/referral.svg" /> Refer Friends
             </div>
-            <div className='coursename'>
-                <ul>
-                    {enrolledcourses.map((courseId, index) => (
-                        <><NextLink href={publicRoutes.curriculum + `/${courseId} `}>
-                            <li key={index}>{courseNameMapping[courseId]}</li>
-                        </NextLink></>
-                    ))}
-                </ul>
-            </div>
+          </a>
+          {/* <div className="svg-feature"><img src="/svg/referral.svg" /> <h6>{referral}</h6> Referral code</div> */}
+          <div className="svg-feature">
+            <img src="/svg/course.svg" /> Enrolled Courses:
+          </div>
         </div>
+        <div className="coursename">
+          <ul>
+            {enrolledcourses.map((courseId, index) => (
+              <>
+                <NextLink href={publicRoutes.curriculum + `/${courseId} `}>
+                  <li key={index}>{courseNameMapping[courseId]}</li>
+                </NextLink>
+              </>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {!setShowButton && (
         <div className="reffer-bttn">
-            <a href="       POP UP with a input to get the referral code and ok bttn     ">
-                <div className="svg-feature"><img src="/svg/celebrate.svg" /> Use referral code</div>
-            </a>
+          <div className="svg-feature cursor-pointer" onClick={showModal}>
+            <img src="/svg/celebrate.svg" /> Use Referral Code
+          </div>
         </div>
+      )}
     </div>
-    );
+  );
 };
 
 export default UserCard;
 
-{/* <NextLink href={publicRoutes.curriculum + `/${courseId} `}></NextLink> */ }
+{
+  /* <NextLink href={publicRoutes.curriculum + `/${courseId} `}></NextLink> */
+}
--- src/interfaces/ui/index.ts
+++ src/interfaces/ui/index.ts
@@ -1,6 +1,9 @@
 export interface IUiState {
-    showNotesModal: boolean;
-    loggedInUser: boolean;
-    loggedInUserId:string;
-    callUserApi:boolean;
+  showNotesModal: boolean;
+  loggedInUser: boolean;
+  loggedInUserId: string;
+  userInfo: any;
+  callUserApi: boolean;
+  showReferralModal: boolean;
+  referralUsed: boolean;
 }
