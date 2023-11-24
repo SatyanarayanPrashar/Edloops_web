4879cd815f2c7219f4268fa3d4804d30c14dd15e
vikas-shivah <vikas@shivawebtech.com>
2023-11-24 12:37:14 +0530

    Change path of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,43 @@
 import Head from "next/head";
-import Landing from "./landing";
+import Landing from "./home";
 
 export default function Home() {
   return (
     <>
       <Head>
-      {/* <!-- HTML Meta Tags --> */}
+        {/* <!-- HTML Meta Tags --> */}
         <title>Edloops</title>
-        <meta name="description" content="Curated Courses to learn tech without spending any money." />
+        <meta
+          name="description"
+          content="Curated Courses to learn tech without spending any money."
+        />
 
-{/* <!-- Google / Search Engine Tags --> */}
+        {/* <!-- Google / Search Engine Tags --> */}
         <meta itemProp="name" content="Edloops" />
-        <meta itemProp="description" content="Curated Courses to learn tech without spending any money." />
+        <meta
+          itemProp="description"
+          content="Curated Courses to learn tech without spending any money."
+        />
         <meta itemProp="image" content="https://i.ibb.co/YNc7SkF/edloops.png" />
 
-{/* <!-- Facebook Meta Tags --> */}
+        {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://edloops.com" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Edloops" />
-        <meta property="og:description" content="Curated Courses to learn tech without spending any money." />
+        <meta
+          property="og:description"
+          content="Curated Courses to learn tech without spending any money."
+        />
         <meta property="og:image" content="" />
 
-{/* <!-- Twitter Meta Tags --> */}
+        {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Edloops" />
-        <meta name="twitter:description" content="Curated Courses to learn tech without spending any money." />
+        <meta
+          name="twitter:description"
+          content="Curated Courses to learn tech without spending any money."
+        />
         <meta name="twitter:image" content="" />
-
       </Head>
       <Landing />
     </>
